9bd91df2d202b12427f31e5f2b751016cbe34e6b
nikolai <nikolai71@msn.com>
2022-08-03 22:31:52 +0300

    start new prorams

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -6,4 +6,28 @@
 //
 // Использование Math.Pow() и аналогов = Ban :)
 
-Console.WriteLine("Hello, World!");
+Console.Write("Введите Число a ");
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите Число b ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+double pow_ab = Math.Pow(a, b);
+  
+Console.WriteLine(pow_ab);
+
+
+// 12. Напишите программу, которая на вход принимает число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет; 46 -> неи; 161 -> да
+// Console.WriteLine("Введите число ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// int mod1 = number1 % 7;
+// if (mod1 == 0)
+// Console.WriteLine("Да");
+// else
+// Console.WriteLine($"нет, остаток = {mod1}");
+// int mod2 = number1 % 23;
+// if (mod2 == 0)
+// Console.WriteLine("Да");
+// else
+// Console.WriteLine($"нет, остаток = {mod2}");
