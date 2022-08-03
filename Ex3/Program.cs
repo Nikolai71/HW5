0371e5211bc846f8e99b9698568bbf9e0662c3d1
nikolai <nikolai71@msn.com>
2022-08-03 21:10:46 +0300

    start new prorams

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//
+// 3, 5 -> 243 (3⁵)
+//
+// 2, 4 -> 16
+//
+// Использование Math.Pow() и аналогов = Ban :)
+
 Console.WriteLine("Hello, World!");
